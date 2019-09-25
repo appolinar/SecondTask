e75b8239f461182c3ae2fcad839f692047eb05dd
agpus <agpushkarev@gmail.com>
2019-09-26 00:25:21 +0300

    items almost fixed. proceed to meadiaplayer

--- app/src/main/java/com/appolinary/msappsecondtaskparsing/presentation/view/VideoActivity.java
+++ app/src/main/java/com/appolinary/msappsecondtaskparsing/presentation/view/VideoActivity.java
@@ -8,13 +8,13 @@ import com.appolinary.msappsecondtaskparsing.R;
 
 public class VideoActivity extends AppCompatActivity {
     private static final String VIDEO_URL = "video url reference";
+    //TODO TUT выровнять кнопку чтобы ровно посередеине под картинкой была
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
 
-        //TODO прилетает интент с адресом, который надо открыть в mediaplayer
         String urlVideo = getIntent().getStringExtra(VIDEO_URL);
     }
 }
